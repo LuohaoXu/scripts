98c77b6b10f77656ee30c9e243d4654b0c91392b
LuohaoXu <1976074396@qq.com>
2023-03-25 20:57:27 +0800

    fix mod_auth_mysqlno match

--- sh/lamp.sh
+++ sh/lamp.sh
@@ -1,4 +1,5 @@
-yum -y install httpd httpd-manual mod_ssl mod_perl mod_auth_mysql unzip  php php-mysql gd php-gd gd-devel php-xml php-common php-mbstring php-ldap php-pear php-xmlrpc php-imap
+yum -y install httpd httpd-manual mod_ssl mod_perl unzip php php-mysql gd php-gd gd-devel php-xml php-common php-mbstring php-ldap php-pear php-xmlrpc php-imap
+yum -y install httpd httpd-devel
 # systemctl start httpd
 rpm -ivh http://dev.mysql.com/get/mysql57-community-release-el7-10.noarch.rpm
 yum -y install mysql-community-server --nogpgcheck
